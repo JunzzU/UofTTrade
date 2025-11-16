b4bd9560d84477e0a1928cb5944950d003a1c194
TutorialBot77 <vboxacct@hotmail.com>
2025-11-16 15:38:21 -0500

    Implemented RegisterController, RegisterState, and RegisterViewModel

--- src/main/java/interface_adapter/register/RegisterViewModel.java
+++ src/main/java/interface_adapter/register/RegisterViewModel.java
@@ -1,4 +1,20 @@
 package interface_adapter.register;
 
-public class RegisterViewModel {
+import interface_adapter.ViewModel;
+
+public class RegisterViewModel extends ViewModel<RegisterState> {
+
+    public static final String TITLE_LABEL = "Register View";
+    public static final String USERNAME_LABEL = "Username:";
+    public static final String PASSWORD_LABEL = "Password:";
+    public static final String REPEAT_PASSWORD_LABEL = "Confirm Password:";
+    public static final String REGISTER_BUTTON_LABEL = "Register";
+    public static final String LOGIN_BUTTON_LABEL = "Login";
+
+
+    public RegisterViewModel() {
+        super("Register");
+        setState(new RegisterState());
+    }
+
 }
